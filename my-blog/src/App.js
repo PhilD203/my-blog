5d90def31334c1e1c8bda433314b364d2fda6cc7
Phil Dugliss <phillipdugliss@gmail.com>
2019-11-06 15:46:16 -0600

    Page routing complete

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
 
 class App extends Component {
   render() {
     return (
+
       <Router>
-        <div className="App">
-          <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <div className="App">
+            <Route path="/" component={HomePage} exact />
+            <Route path="about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/article" component={ArticlePage} />
+          </div>
         </div>
       </Router>
+
     );
   }
 }
