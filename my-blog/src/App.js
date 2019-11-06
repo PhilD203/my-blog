7cf8e6957500f9486fd4ed5dcb1753d12eaed21c
Phil Dugliss <phillipdugliss@gmail.com>
2019-11-06 15:58:00 -0600

    NavBar Component Created
    NavBar componet created
    CSS Added
    Nav links working

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 
 class App extends Component {
@@ -12,13 +13,14 @@ class App extends Component {
     return (
 
       <Router>
-        <div id="page-body">
           <div className="App">
-            <Route path="/" component={HomePage} exact />
-            <Route path="about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesList} />
-            <Route path="/article" component={ArticlePage} />
-          </div>
+            <NavBar/>
+            <div id="page-body">
+              <Route path="/" component={HomePage} exact />
+              <Route path="about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesList} />
+              <Route path="/article" component={ArticlePage} />
+            </div>
         </div>
       </Router>
 
